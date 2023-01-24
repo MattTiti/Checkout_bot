76bd34c394289daecda919f67b99fa305533d386
MattTiti <mtiti3@vt.edu>
2023-01-24 12:54:38 -0500

    w/ docs

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer');
 const product_url = "https://telfar.net/collections/beanies/products/embroidered-beanie-white-white"
 
+/**
+ * Creates a chromium page controlled by puppeteer.
+ * @returns A chromium page controlled by puppeteer.
+ */
 async function givePage() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
@@ -8,6 +12,10 @@ async function givePage() {
     return page
 }
 
+/**
+ * Adds item to cart and navigates to cart.
+ * @param {*} page The chromium page being controlled by puppeteer.
+ */
 async function addToCart(page){
     await page.goto(product_url);
     // await page.select('#product-template_un-product-select-option-1', 'L');
@@ -17,7 +25,13 @@ async function addToCart(page){
     await page.click("#view-cart", elem => elem.click());
     await page.waitForSelector("button[class='btn cart-checkout-btn']");
     await page.click("button[class='btn cart-checkout-btn']", elem => elem.click());
-    
+} 
+
+/**
+ * Fills out shipping information and navigates to the payment page .
+ * @param {*} page The chromium page being controlled by puppeteer.
+ */
+async function fillShipping(page){
     await page.waitForNavigation();
     await page.waitForTimeout(500);
     await page.type('#checkout_email', 'mtiti3@vt.edu');
@@ -40,20 +54,18 @@ async function addToCart(page){
     await page.type('#checkout_shipping_address_phone', '6');
     await page.click("#continue_button", elem => elem.click());
 
+}
+/**
+ * Fills out payment information and places order.
+ * @param {*} page The chromium page being controlled by puppeteer.
+ */
+async function fillPayment(page) {
     await page.waitForNavigation();
     await page.waitForTimeout(500);
     await page.click("#continue_button", elem => elem.click());
-
     await page.waitForNavigation();
-    // await page.waitForSelector("#card-fields-number-aak7x0c9s2f00000-scope-telfar.net");
-
-    // await page.type('#card-fields-number-aak7x0c9s2f00000-scope-telfar.net', '4266902037251497');
-    // await page.type('#card-fields-name-0ce3kdsodf0m0000-scope-telfar.net', 'Matthew Titi');
-    // await page.type('#card-fields-expiry-v9o54ylfn6000000-scope-telfar.net', '04/22');
-    // await page.type('#card-fields-verification_value-ohtq0v8v4yo00000-scope-telfar.net', '333');
-    // await page.click("#continue_button", elem => elem.click());
-
     await page.waitForTimeout(500);
+
     const iframeNum = await page.frames().find(frame => frame.name().includes('card-fields-number'));
     const iframeName = await page.frames().find(frame => frame.name().includes('card-fields-name'));
     const iframeExp = await page.frames().find(frame => frame.name().includes('card-fields-expiry'));
@@ -68,12 +80,16 @@ async function addToCart(page){
 
     await page.click("#checkout_different_billing_address_false", elem => elem.click());
     await page.click("#continue_button", elem => elem.click());
+}
 
-} 
-
+/**
+ * This is the main method of the program that checks out the item.
+ */
 async function checkout() {
     var page = await givePage();
     await addToCart(page);
+    await fillShipping(page);
+    await fillPayment(page);
 }
 
 checkout();
